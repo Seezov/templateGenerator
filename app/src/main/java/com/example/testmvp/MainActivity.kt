5fded41880ce3e8f7ce487986bef424919a00160
Oleksandr Sizov <oleksandr.sizov@ekreative.com>
2020-12-18 01:30:26 +0200

    Remove recycler activity

--- app/src/main/java/com/example/testmvp/MainActivity.kt
+++ app/src/main/java/com/example/testmvp/MainActivity.kt
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        startActivity(Intent(this, RecyclerViewActivity::class.java))
+        //startActivity(Intent(this, RecyclerViewActivity::class.java))
     }
 }
